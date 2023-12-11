88696149d9b30f8c3c42f623c01209ece1862e0f
public <public@copp.local>
2023-12-11 13:57:56 +0700

    add pdf

---
+++ Лабараторная №3/ConsoleApplication1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.ComponentModel;
+using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+internal class Program
+{
+    public static void Main(string[] args)
+    {
+        try
+        {
+            string[] lines = File.ReadAllLines("input.txt");
+            int[] chosenNumbers = lines[0].Split(' ').Select(int.Parse).ToArray();
+            int numberOfTickets = int.Parse(lines[1]);
+            using (StreamWriter writer = new StreamWriter("output.txt"))
+            {
+                for (int i = 2; i < 2 + numberOfTickets; i++)
+                {
+                    int[] ticketNumbers = lines[i].Split(' ').Select(int.Parse).ToArray();
+                    int matches = ticketNumbers.Intersect(chosenNumbers).Count();
+
+                    writer.WriteLine(matches >= 3 ? "Lucky" : "Unlucky");
+                }
+            }
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"An error occorred: {ex.Message}");
+            throw;
+        }
+    }
+}
---
+++ Лабараторная №3/ConsoleApplication2/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.ComponentModel;
+using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+internal class Program
+{
+    public static void Main(string[] args)
+    {
+        try
+        {
+            string[] lines = File.ReadAllLines("nums.txt");
+            for (int i = 0; i < lines.Length; i++)
+            {
+                string[] numbers = lines[i].Split(' ');
+                lines[i] = string.Join(" ", numbers.Where(number => isEven(number)));
+            }
+
+            File.WriteAllLines("nums.txt", lines);
+            Console.WriteLine("Четные числа удалены успешно.");
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"An error occorred: {ex.Message}");
+            throw;
+        }
+    }
+
+    static bool isEven(string number)
+    {
+        if (int.TryParse(number, out int num))
+        {
+            return num % 2 != 0;
+        }
+
+        return true;
+    }
+}
